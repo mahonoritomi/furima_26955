163ed5ead2ad30fd1e11ef86be41641662dcdbb7
mahonoritomi <maho.noritomi@gmail.com>
2020-07-11 18:21:32 +0900

    add selling function

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,13 @@ class Item < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
 
   with_options presence: true do
     validates :image
     validates :name
     validates :explanation
-    validates :price,          format: { with: /\A3[0-9][0-9]|[1-9][0-9][0-9][0-9][0-9][0-9][0-9]+\z/, message: "300以上9,999,999以下の半角数字で入力してください" } 
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }          
     validates :user
   
   end
@@ -27,13 +28,6 @@ class Item < ApplicationRecord
       validates :transport_days
     end
 
-    with_options numericality: { other_than: 1 } do
-      validates :category
-      validates :condition
-      validates :delivery_fee
-      validates :ship_from
-      validates :transport_days
-    end
 
 
 
---
+++ spec/models/item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+
+
+
+
+  
+end
